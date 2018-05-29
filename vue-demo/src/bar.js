7586fd26ca0e94b7543a6f551a86c63058d45f03
ZWQ <zhong0102@qq.com>
2018-05-30 04:06:15 +0800

    vue-demo-todoList

---
+++ vue-demo/src/bar.js
@@ -0,0 +1,4 @@
+export default function bar() {
+  let a = 2;
+  console.log(a)
+}
---
+++ vue-demo/webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders:[
+      {
+        test: /\.js[x]?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader?presets[]=es2015&presets[]=react'
+      },
+    ]
+  },
+  resolve: {
+    alias: {
+      'vue$': 'vue/dist/vue.common.js'
+    }
+  }
+};
